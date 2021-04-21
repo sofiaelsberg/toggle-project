00de99220aa6178fe6fe469ea7d3aefceb1d4873
sofiaelsberg <56071273+sofiaelsberg@users.noreply.github.com>
2021-04-21 13:37:39 +0200

    adding more opt to toggle functionality and icons

--- src/partials/navigation.js
+++ src/partials/navigation.js
@@ -4,27 +4,7 @@ function toggleMenu() {
     console.log("script is imported and executed");
 
 
-    // Navigation opt1
-
-    // const navLinks = document.querySelector('.nav-links');
-    // const burgerToggle = document.querySelector('#burger');
-
-
-    // openMenu.addEventListener('click', show);
-    // openMenu.addEventListener('click', close);
-
-    // function show() {
-    //     navLinks.style.display = 'flex';
-    //     burgerToggle.style.color = 'black';
-    // }
-
-    // function close() {
-    //     navLinks.style.display = 'none';
-    // }
-
-
-
-    // Navigation opt2  - using nullcheck - cannot close
+    // Navigation opt1  - using nullcheck
 
     // const navLinks = document.querySelector('.nav-links');
     // const burgerToggle = document.querySelector('#burger');
@@ -32,7 +12,7 @@ function toggleMenu() {
     // burgerToggle.addEventListener('click', (e) => {
     //     e.preventDefault();
 
-    //     if (window.getComputedStyle(navLinks).display === 'block') {
+    //     if (window.getComputedStyle(navLinks).display === 'flex') {
     //         navLinks.style.display = 'none';
     //     } else {
     //         navLinks.style.display = 'flex';
@@ -41,17 +21,54 @@ function toggleMenu() {
     // });
 
 
-    // Navigation opt3 - using toggle
+    // Navigation opt2 - using eventlistener click and toggle classList
 
-    const burgerToggle = document.querySelector('#burger');
+    // const burgerToggle = document.querySelector('#burger');
+    // const navLinks = document.querySelector('.nav-links');
+
+    // // Checks for visibility and adds classes accordingly
+
+    // burgerToggle.addEventListener('click', () => {
+    //     navLinks.classList.toggle('show');
+    //     burgerToggle.classList.toggle('burger-open');
+    // })
+
+    // Navigation opt3 - using onclick instead og evenlistener
+
+    // const burgerToggle = document.querySelector('#burger, #xicon');
+    // // const changeIcon = document.querySelector('#xicon');
+    // const navLinks = document.querySelector('.nav-links');
+
+    // burgerToggle.onclick = function () {
+    //     navLinks.classList.toggle('active');
+    //     burgerToggle.classList.toggle('burger-open');
+    //     // burgerToggle.classList.toggle('hide-burger');
+    //     // changeIcon.classList.toggle('show-x');
+    // }
+
+
+    //   Navigation opt4 - using eventlisteners and inline styling.... - works but very fucking ugly piece of code and unnecessary complicated
+
     const navLinks = document.querySelector('.nav-links');
+    const burgerToggle = document.querySelector('#burger');
+    const changeIcon = document.querySelector('#xicon');
 
-    // Checks for visibility and adds classes accordingly
 
-    burgerToggle.addEventListener('click', () => {
-        navLinks.classList.toggle('show');
-        burgerToggle.classList.toggle('open-color');
-    })
+    burgerToggle.addEventListener('click', show);
+    changeIcon.addEventListener('click', close);
 
+    function show() {
+        navLinks.style.display = 'flex';
+        burgerToggle.style.color = 'black';
+        burgerToggle.classList.toggle('hide-burger');
+        changeIcon.classList.toggle('show-x');
+    }
+
+    function close() {
+        navLinks.style.display = 'none';
+        burgerToggle.style.color = 'white';
+        burgerToggle.classList.toggle('hide-burger');
+        changeIcon.classList.toggle('show-x');
+    }
 }
 export { toggleMenu };
